6a9351ca550330886e14597fe8b7fece4b35d10f
omerserkanicel <135071032+omerserkanicel@users.noreply.github.com>
2023-05-31 23:21:41 +0300

    Update sozluk.py

--- sozluk.py
+++ sozluk.py
@@ -1,1 +1,9 @@
-
+sozluk = {"kanki": "sevdiğin kişiye sesleniş şekli","denyo": "MALL!!!","manita": "sevgili",
+        "bug": "oyun hatası"}
+        
+        
+secim = input("Kanki,Denyo,Manita,Bug bunlardan hangisinin anlamını öğrenmek istersin:")
+if secim in sozluk:
+    print(sozluk[secim])
+else:
+    print("sözlükte böyle bir kelime yok")
